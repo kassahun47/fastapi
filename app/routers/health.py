3c66ceda711c9f097c2bb61c75476956ea0b7376
kassahun47 <kassahunzewdu47@gmail.com>
2025-10-15 15:58:00 +0300

    Create health.py

---
+++ app/routers/health.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, status
+from models.health import Health
+
+router = APIRouter(
+    prefix='/api/ping',
+    tags=['Health Check']
+)
+
+@router.get('/', response_model=Health, status_code=status.HTTP_200_OK)
+def health_check() -> str:
+    return Health
